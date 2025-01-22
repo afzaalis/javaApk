65a601d489f5b0920bf9b8ba2c1bfa64133c02cd
Afzaal Isnaufal <70215760+afzaalis@users.noreply.github.com>
2025-01-22 15:21:53 +0700

    gui & jdbc test

---
+++ src/Database/DatabaseConnection.java
@@ -0,0 +1,21 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            String url = "jdbc:mysql://localhost:3307/javaApk"; 
+            String username = "root"; 
+            String password = "";
+            
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
